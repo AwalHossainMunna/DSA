29821d2bd17955ab36f4ca7e6976d918bf756d3b
Munna <111683554+AwalHossainMunna@users.noreply.github.com>
2023-03-03 20:05:01 +0600

    Code

---
+++ Algorithms/Searching/Binary Search/SquareRoot.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int sqroot(int a){
+    int s = 0, e = a;
+    int mid = s+(e-s)/2;
+    int ans=-1;
+    
+    while(s<=e){
+
+        int sqr=mid*mid;
+
+        if(sqr==a){
+            return mid;
+        }
+        else if(sqr<a){
+            ans=mid;
+            s=mid+1;  
+        }
+        else{
+            e=mid-1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
+int main()
+{
+    int num;
+    cin>>num;
+    cout<<sqroot(num);
+    return 0;
+}
