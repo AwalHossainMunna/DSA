17ee59289d1629495e77d5f0360e764cf9b19ba7
Munna <mygames.x20@gmail.com>
2023-03-15 23:57:52 +0600

    Code

--- Algorithms/Sorting/InsertionSort.cpp
+++ Algorithms/Sorting/InsertionSort.cpp
@@ -17,9 +17,11 @@ using namespace std;
 void insertionSort(int arr[], int n)
 {
     int i = 1;
+    
     while (i < n)
     {
         int temp = arr[i];
+        
         int j = i - 1;
 
         while (j >= 0)
@@ -32,10 +34,12 @@ void insertionSort(int arr[], int n)
             {
                 break;
             }
+            
             j--;
         }
 
         i++;
+        
         arr[j + 1] = temp;
     }
 }
