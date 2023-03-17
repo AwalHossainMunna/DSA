76ae3e2fad47020df45d4acfb630c508d79ab38a
Munna <mygames.x20@gmail.com>
2023-03-17 15:18:20 +0600

    Code

--- Data-Structure/Array/ReverseArray.cpp
+++ Data-Structure/Array/ReverseArray.cpp
@@ -1,29 +1,38 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+void reverseArray(int arr[], int n){
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        int e = n - i;
+        int temp = arr[i];
+        arr[i] = arr[e - 1];
+        arr[e - 1] = temp;
+    }    
+}
+
+void printArray(int arr[], int n){
+
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
 
 int main()
 {
     int size;
-    scanf("%d", &size);
+    cin>>size;
     int arr[size];
 
     for (int i = 0; i < size; i++)
     {
-        scanf("%d", &arr[i]);
+        cin>>arr[i];
     }
 
-    for (int i = 0; i < size / 2; i++)
-    {
-        int e = size - i;
-        int temp = arr[i];
-        arr[i] = arr[e - 1];
-        arr[e - 1] = temp;
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+    reverseArray(arr,size);
+    printArray(arr,size);
 
     return 0;
 }
