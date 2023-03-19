6e9b46669680bc8e5420e8b38f282f07097b98ac
Munna <mygames.x20@gmail.com>
2023-03-19 23:10:59 +0600

    Code

---
+++ Data-Structure/Array/RotateArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void rotateArray(int arr[], int n, int k)
+{
+
+    int temp[n];
+    for (int i = 0; i < n; i++)
+    {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    // copy temp into arr array;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+}
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int size, k;
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> k;
+    rotateArray(arr, size, k);
+    print(arr, size);
+
+    return 0;
+}
