522a55a40e37253558cc75bf767f5a3e780a5883
Munna <mygames.x20@gmail.com>
2023-03-22 00:23:20 +0600

    Code

--- Algorithms/Searching/Binary Search/SquareRoot.cpp
+++ Algorithms/Searching/Binary Search/SquareRoot.cpp
@@ -1,34 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int squareRoot(int a){
+int squareRoot(int a)
+{
     int s = 0, e = a;
-    int mid = s+(e-s)/2;
-    int ans=-1;
-    
-    while(s<=e){
+    int mid = s + (e - s) / 2;
+    int ans = -1;
 
-        int sqr=mid*mid;
+    while (s <= e)
+    {
 
-        if(sqr==a){
+        int sqr = mid * mid;
+
+        if (sqr == a)
+        {
             return mid;
         }
-        else if(sqr<a){
-            ans=mid;
-            s=mid+1;  
+        else if (sqr < a)
+        {
+            ans = mid;
+            s = mid + 1;
         }
-        else{
-            e=mid-1;
+        else
+        {
+            e = mid - 1;
         }
-        mid = s+(e-s)/2;
+        mid = s + (e - s) / 2;
     }
     return ans;
 }
 
+
 int main()
 {
     int num;
-    cin>>num;
-    cout<<squareRoot(num);
+    cin >> num;
+    cout << squareRoot(num);
     return 0;
 }
+
