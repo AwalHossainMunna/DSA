3fe37b95f9a64ddef1f5c0d8ebbd780e50a48636
Munna <mygames.x20@gmail.com>
2023-03-22 00:06:31 +0600

    Code

--- Data-Structure/Array/MaxMin.cpp
+++ Data-Structure/Array/MaxMin.cpp
@@ -1,38 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int maxElement(int a[], int n){
-    int max=a[0];
+int maxElement(int a[], int n)
+{
+    int max = a[0];
 
-    for(int i=1; i<n; i++){
-        if(a[i]>max){
-            max=a[i];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
         }
     }
     return max;
 }
 
-int minElement(int a[], int n){
-    int min=a[0];
+int minElement(int a[], int n)
+{
+    int min = a[0];
 
-    for(int i=1; i<n; i++){
-        if(a[i]<min){
-            min=a[i];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
         }
     }
     return min;
 }
 
+
 int main()
 {
     int size;
-    cin>>size;
+    cin >> size;
     int arr[size];
 
-    for(int i=0; i<size; i++){
-        cin>>arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
-    cout<<"Minimum element is: "<<minElement(arr,size)<<endl;
-    cout<<"Maximum element is: "<<maxElement(arr,size)<<endl;
+    cout << "Minimum element is: " << minElement(arr, size) << endl;
+    cout << "Maximum element is: " << maxElement(arr, size) << endl;
     return 0;
 }
