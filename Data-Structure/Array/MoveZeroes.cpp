5655ae69e34028a3f21cb45213430c463384598d
Munna <mygames.x20@gmail.com>
2023-03-22 00:03:47 +0600

    Code

--- Data-Structure/Array/MoveZeroes.cpp
+++ Data-Structure/Array/MoveZeroes.cpp
@@ -5,7 +5,7 @@ void moveZeros(int arr[], int n)
 {
     int i = 0;
 
-    for (int j=0; j<n; j++)
+    for (int j = 0; j < n; j++)
     {
         if (arr[j] != 0)
         {
@@ -17,11 +17,10 @@ void moveZeros(int arr[], int n)
     }
 }
 
-
 void print(int arr[], int n)
 {
 
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
@@ -34,7 +33,7 @@ int main()
     cin >> size;
     int arr[size];
 
-    for (int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
