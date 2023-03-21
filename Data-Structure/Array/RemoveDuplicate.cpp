3336e4b03d12e28aa0b58e6a1686112562046c25
Munna <mygames.x20@gmail.com>
2023-03-22 00:00:21 +0600

    Code

--- Data-Structure/Array/RemoveDuplicate.cpp
+++ Data-Structure/Array/RemoveDuplicate.cpp
@@ -1,34 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void removeDuplicate(int arr[],int n){
+void removeDuplicate(int arr[], int n)
+{
 
-    for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            if(arr[i]==arr[j]){
-                for(int k=j; k<n; k++){
-                    arr[k]=arr[k+1];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                for (int k = j; k < n; k++)
+                {
+                    arr[k] = arr[k + 1];
                 }
                 j--;
                 n--;
-            }  
+            }
         }
     }
 
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }   
 }
+
+
 
 int main()
 {
     int size;
-    cin>>size;
+    cin >> size;
     int arr[100];
 
-    for(int i=0; i<size; i++){
-        cin>>arr[i];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
-    removeDuplicate(arr,size);
+    removeDuplicate(arr, size);
+    
     return 0;
 }
