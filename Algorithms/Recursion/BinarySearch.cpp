da3c5526a192745f8c279dff12dbc0f17e934973
Munna <mygames.x20@gmail.com>
2023-04-10 16:14:20 +0600

    Code

---
+++ Algorithms/Recursion/BinarySearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int s, int e, int target)
+{
+    if (s > e)
+    {
+        return -1;
+    }
+
+    int mid = s + (e - s) / 2;
+
+    if (arr[mid] == target)
+    {
+        return mid;
+    }
+    
+    if (target < arr[mid])
+    {
+        return binarySearch(arr, s, mid - 1, target);
+    }
+
+    return binarySearch(arr, mid + 1, e, target);
+}
+
+int main()
+{
+    int size, target;
+    cin >> size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> target;
+
+    int ans = binarySearch(arr, 0, size - 1, target);
+    cout << ans;
+
+    return 0;
+}
