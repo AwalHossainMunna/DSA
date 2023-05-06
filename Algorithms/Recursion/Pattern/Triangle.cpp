de7d8a10e7c383f64df7a6b407473ac7b5fc0141
Munna <awalhsnmunna@gmail.com>
2023-05-06 15:25:16 +0600

    Code

---
+++ Algorithms/Recursion/Pattern/Triangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void triangle(int r, int c)
+{
+    if (r == 0)
+    {
+        return;
+    }
+    if (c < r)
+    {
+        cout << "*";
+        triangle(r, c + 1);
+    }
+    else
+    {
+        cout << endl;
+        triangle(r - 1, 0);
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    triangle(N, 0);
+
+    return 0;
+}
