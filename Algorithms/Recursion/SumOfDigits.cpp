fcde00e40f6085d794f09ded1105d902a32a8243
Munna <awalhsnmunna@gmail.com>
2023-06-07 15:32:24 +0600

    Code

---
+++ Algorithms/Recursion/SumOfDigits.cpp
@@ -0,0 +1,27 @@
+
+include<bits/stdc++.h>
+
+int sumDigit(int n){
+    
+    // Base call
+    if(n==0){
+        return n;
+    }
+
+    int digit=n%10;
+
+    return digit+sumDigit(n/10);
+
+}
+
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+
+    int ans = sumDigit(N);
+    cout<<ans;
+
+    return 0;
+}
