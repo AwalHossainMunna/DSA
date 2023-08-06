9510ec2fc3e6b0740bb7ef4b0ffa55590e43d32a
Munna <awalhsnmunna@gmail.com>
2023-08-06 17:40:09 +0600

    Code

--- Data-Structure/Linked List/LinkedListInsertion.cpp
+++ Data-Structure/Linked List/LinkedListInsertion.cpp
@@ -5,11 +5,10 @@ class Node{
     public:
     int data;
     Node *next;
- 
-    //constructor
-    Node(int data){
-        data= data;
-        next= NULL;
+
+    Node(int val){
+        data = val;
+        next = NULL;
     }
 };
 
@@ -17,19 +16,18 @@ class Node{
 // Function to insert at beginning
 void insertAtBeginning(Node* &head, int d){
     //new node create 
-    Node* temp = new Node(d);
-    temp->next = head;
-    head = temp;
+    Node* newNode = new Node(d);
+    newNode->next = head;
+    head = newNode;
 }
 
 
 // Function to insert at End
 void insertAtEnd(Node* &tail, int d){
 
-    //new node create 
-    Node* temp = new Node(d);
-    tail->next = temp;
-    tail = temp;
+    Node* newNode = new Node(d);
+    tail->next = newNode;
+    tail = newNode;
 }
 
 
@@ -57,7 +55,6 @@ void insertAtPosition(Node* &head, Node* &tail, int position, int d){
         i++;
     }
 
-    // Create new node
     Node* newNode = new Node(d);
 
     newNode->next = temp->next;
@@ -86,7 +83,7 @@ int main()
 
     insertAtBeginning(head,12);
     insertAtEnd(tail,16);
-    insertAtPosition(head,tail,4,22);
+    insertAtPosition(head,tail,2,22);
 
     print(head);
 
