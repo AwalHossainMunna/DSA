283f368dec36f3e3dbdcae78969fbf85fd113351
Munna <awalhsnmunna@gmail.com>
2023-08-12 11:30:05 +0600

    Code

--- Data-Structure/Linked List/Doubly Linked List/LinkedListInsertion.cpp
+++ Data-Structure/Linked List/Doubly Linked List/LinkedListInsertion.cpp
@@ -15,7 +15,6 @@ class Node{
     }
 };
 
-
 class DoublyLinkedList{
     public:
 
@@ -27,25 +26,38 @@ class DoublyLinkedList{
         tail = NULL;
     }
 
-    void insertAtStart(int val){
+    void insertAtHead(int d){
 
-        Node* newNode = new Node(val);
+        Node* newNode = new Node(d);
         if(head == NULL){
             head = newNode;
             tail = newNode;
             return;
         }
 
-        newNode->next = head;
         head->prev = newNode;
+        newNode->next = head;
         head = newNode;
         return;
     }
 
+    void insertAtEnd(int d){
+
+        Node* newNode = new Node(d);
+        if(tail == NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+        return;
+    }
 
     void display(){
 
-        Node* temp;
+        Node* temp = head;
         cout<<"NULL<->";
         while(temp != NULL){
             cout<<temp->data<<"<->";
@@ -55,17 +67,18 @@ class DoublyLinkedList{
 };
 
 
-
 int main()
-{ 
-
+{
     DoublyLinkedList dll;
-    
-    dll.insertAtStart(1);
-    dll.insertAtStart(2);
-    dll.insertAtStart(3);
+    dll.insertAtHead(5);
+    dll.insertAtHead(4);
+    dll.insertAtHead(3);
+
+    dll.insertAtEnd(6);
+    dll.insertAtEnd(7);
+    dll.insertAtEnd(8);
 
     dll.display();
-
+    
     return 0;
 }
