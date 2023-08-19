9b0236131f439d15f4def30211efbbdb4192ab68
Munna <awalhsnmunna@gmail.com>
2023-08-19 17:25:40 +0600

    Code

---
+++ Data-Structure/Linked List/Singly Linked LIst/CreateLinkedList.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+
+class SinglyLinkedList{
+    public:
+
+    Node* head;
+    Node* tail;
+
+    SinglyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL";
+    }
+};
+
+
+int main()
+{
+
+    SinglyLinkedList sll;
+
+    sll.display();
+
+    return 0;
+}
