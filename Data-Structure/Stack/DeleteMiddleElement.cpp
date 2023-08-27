ec4ad24aa7b92090037e07e14d592a2a91364bbb
Munna <awalhsnmunna@gmail.com>
2023-08-27 20:19:05 +0600

    Code

---
+++ Data-Structure/Stack/DeleteMiddleElement.cpp
@@ -0,0 +1,48 @@
+
+/* Question: https://www.geeksforgeeks.org/delete-middle-element-stack/ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void deleteMiddle(stack<int> &stack, int count,int size){
+
+    // base case
+    if(count == size){
+        return;
+    }
+
+    //store recursive top element in num
+    int num = stack.top();
+    stack.pop();
+    
+    //Recursive call
+    deleteMiddle(stack,count+1,size);
+    stack.push(num);
+}
+
+
+int main()
+{
+    stack<int> s;
+    int size;
+    cin>>size;
+
+    for(int i=0; i<size; i++){
+        int x;
+        cin>>x;
+        s.push(x);
+    }
+
+    size = s.size()/2;
+
+    deleteMiddle(s,0,size);
+
+    while(!s.empty()){
+        int x = s.top();
+        cout<<x<<" ";
+        s.pop();
+    }
+
+    return 0;
+}
